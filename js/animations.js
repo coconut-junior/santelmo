1e9e622e4f09b31defae154d45b84a6e59a15be6
Jimmy Blanck <blanckjm@gmail.com>
2021-10-23 20:28:31 -0400

    changed background

--- js/animations.js
+++ js/animations.js
@@ -5,13 +5,11 @@ gsap.from("#santelmo",{opacity:0,y:50,duration:1,delay:2})
 gsap.from("#blob",{x:-300,duration: 1,delay:1})
 
 gsap.set("#blob", {
-
-  transformOrigin:"90px 90px"         // make transform origin be center for x and y axis
+  transformOrigin:"90px 90px"
 });
 gsap.to("#blob",{rotation:360,duration:30,repeat:-1,ease:"power1.out"})
 
 gsap.set("#tail", {
-
   rotation: 0,
   transformOrigin:"100% 0%"         // make transform origin be center for x and y axis
 });
@@ -20,6 +18,6 @@ gsap.to("#tail",{rotation:10,ease:Linear.circ,duration:1,repeat:-1,yoyo:true})
 var width = 1100, //assumes your image is 1024px wide
    speed = -10, //pixels per second
    duration = 10,
-   endPosition = -220; //adjust the end position assuming 60fps
+   endPosition = -220;
 gsap.to("#background", duration, {css:{backgroundPosition:endPosition + "px " + endPosition + "px"}, repeat:-1,yoyo:true});
 
